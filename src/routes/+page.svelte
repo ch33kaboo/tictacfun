a70aed1bcf47b848bd5219d455446aa27736fee9
ch33kaboo <mc.ouzane@esi-sba.dz>
2025-04-09 10:54:10 +0100

    minor styling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -72,9 +72,9 @@
 	}
 </script>
 
-<main class="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center py-8">
+<main class="mt-10 flex flex-col items-center justify-start">
 	<h1 class="mb-8 text-center text-4xl" style="font-family: 'milkyway', sans-serif;">
-		Invite your friends and play Tic-Tac-Toe now
+		Invite your friends and play Tic Tac Toe now
 	</h1>
 
 	<div class="bg-base-200 w-full max-w-2xl rounded-lg p-6 shadow-xl">
